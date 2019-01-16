28a46dc2c11e833e5c220afdebb71f67cc2b1c6f
thomas.blt <36473514+thomasblot1@users.noreply.github.com>
2019-01-16 17:20:55 +0100

    Page java apres la connection, création de la page , import et extands

---
+++ MainFeature/src/main/java/ppeteam/myapplication/MainFeature/Pageconnecte.java
@@ -0,0 +1,9 @@
+package ppeteam.myapplication.MainFeature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class Pageconnecte extends AppCompatActivity{
+}

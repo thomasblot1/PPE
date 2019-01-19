7b20d5dc965c0b8d1d7abe44281898ea850cf6c3
thomas.blt <36473514+thomasblot1@users.noreply.github.com>
2019-01-19 14:30:32 +0100

    [dataHelper] je commence la connexion avec la mini base de donnée situé dans la cache du telelphone

--- MainFeature/src/main/java/ppeteam/myapplication/MainFeature/DatabaseHelper.java
+++ MainFeature/src/main/java/ppeteam/myapplication/MainFeature/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package ppeteam.myapplication.MainFeature;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
@@ -11,24 +12,32 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
     private static String TAG="DatabaseHelper";
-    private static String TABLE_NAME="Event";
+    private static String TABLE_NAMEevent="Event";
     private static String COL1="ID_recette";
     private static String COL2="ID_Utilisateur";
     private static String COL3="chemin_l_imagine_event";
     private static String COL4="lieu";
     private static String COL5="recette";
-    private static ;
+
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTable= ;
     }
+        String createtableevent =" CREATE TABLE" + TABLE_NAMEevent + "(ID_recette PRIMARY KEY" + COL2 + "TEXT ,"+ COL3 +"TEXT,"+COL4+"TEXT"+COL5+"TEXT)";
+    db.execSQL(createtableevent);
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP IF TABLE EXISTS"+ TABLE_NAMEevent);
+        onCreate(db);
     }
     public DatabaseHelper(Context context){
         super(context, TABLE_NAME, null);
+    }
+    public boolean addevent(String recette,String lieu,String chemin_l_imagine_event){
+        SQLiteDatabase db =this.getWritableDatabase();
+        ContentValues value= new ContentValues();
+        value.put()
     }
 }
